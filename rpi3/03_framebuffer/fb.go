3b18413348cfb4e6413a45b01d0dc87434164f85
Ian Smith <iansmith@acm.org>
2019-09-22 11:38:12 -0400

    added support for moving font into the binary in 03

--- rpi3/03_framebuffer/fb.go
+++ rpi3/03_framebuffer/fb.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	dev "device/rpi3"
+	"unsafe"
 )
 
 func main() {
@@ -12,5 +13,22 @@ func main() {
 	if !dev.InitFramebuffer() {
 		print("unable to init framebuffer!")
 	}
+	font := (*psf)(unsafe.Pointer(&_binary_font_psf_start))
+	print("magic=", font.magic, " headersize=", font.headersize, " height=", font.height, " width=", font.width, "bytesperglyph=", font.bytesperglyph, " \n")
 	print("done\n")
+}
+
+//go:extern _binary_font_psf_start
+var _binary_font_psf_start *uint8
+
+type psf struct {
+	magic         uint32
+	version       uint32
+	headersize    uint32
+	flags         uint32
+	numglyph      uint32
+	bytesperglyph uint32
+	height        uint32
+	width         uint32
+	glyphs        *uint8
 }
